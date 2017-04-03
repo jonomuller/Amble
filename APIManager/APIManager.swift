0d4b5b96e33e4f49b97082c765d05f7a63c74382
Jono Muller <jonomuller@gmail.com>
2017-04-03 14:42:47 +0100

    Create skeleton APIManager file to handle requests to server

---
+++ APIManager/APIManager.swift
@@ -0,0 +1,17 @@
+//
+//  APIManager.swift
+//  amble-ios
+//
+//  Created by Jono Muller on 03/04/2017.
+//  Copyright © 2017 Jonathan Muller. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class APIManager: NSObject {
+    
+    
+
+}
