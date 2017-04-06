edfad691003dd16d3cc4c92dfbc70d15b7a95acf
Jono Muller <jonomuller@gmail.com>
2017-04-06 18:17:54 +0100

    Write tests for login and register APIManager functions

--- amble-iosTests/APIManagerTests.swift
+++ amble-iosTests/APIManagerTests.swift
@@ -7,29 +7,60 @@
 //
 
 import XCTest
+import SwiftyJSON
+@testable import amble_ios
 
 class APIManagerTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  // MARK: POST /login
+  
+  func testLoginWithNoDetailsReturnsError() {
+    let exp = expectation(description: "POST /login no details")
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    APIManager.sharedInstance.login(with: [:]) { (json, error) in
+      XCTAssert(type(of: json) == JSON.self, "Data returned is not of type JSON")
+      XCTAssert(error != nil, "Error is nil")
+      let success = json["success"].boolValue
+      XCTAssert(!success, "Success value is true")
+      XCTAssert(error?.localizedDescription == "Missing credentials")
+      exp.fulfill()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    waitForExpectations(timeout: 5, handler: nil)
+  }
+  
+  // MARK: POST /register
+  
+  func testRegisterWithNoDetailsReturnsError() {
+    let exp = expectation(description: "POST /register no details")
+    
+    APIManager.sharedInstance.register(with: [:]) { (json, error) in
+      XCTAssert(type(of: json) == JSON.self, "Data returned is not of type JSON")
+      XCTAssert(error != nil, "Error is nil")
+      let success = json["success"].boolValue
+      XCTAssert(!success, "Success value is true")
+      XCTAssert(error?.localizedDescription == "Please enter your username.")
+      exp.fulfill()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    waitForExpectations(timeout: 5, handler: nil)
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
