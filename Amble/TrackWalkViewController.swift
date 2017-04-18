aa0dd3207eb3a53fcdb9ad0c62e41aac66ef8801
Jono Muller <jonomuller@gmail.com>
2017-04-18 18:28:45 +0100

    Add button to display user's current location on map

--- Amble/TrackWalkViewController.swift
+++ Amble/TrackWalkViewController.swift
@@ -7,27 +7,31 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 
 class TrackWalkViewController: UIViewController {
   
-  var locationManager: CLLocationManager!
+  @IBOutlet var mapView: MKMapView!
+  fileprivate var locationManager: CLLocationManager!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let locationButton = MKUserTrackingBarButtonItem(mapView:self.mapView)
+    self.navigationItem.leftBarButtonItem = locationButton
     
     locationManager = CLLocationManager()
     locationManager.delegate = self
     locationManager.activityType = .fitness
     locationManager.desiredAccuracy = kCLLocationAccuracyBest
     
-    let status = CLLocationManager.authorizationStatus()
-    
-    // Request user's location, if permission is denied then display error
-    if status == .notDetermined {
-      locationManager.requestAlwaysAuthorization()
-    } else if status == .denied {
-      displayLocationError()
+    requestLocation()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    if CLLocationManager.authorizationStatus() == .authorizedAlways {
+      startTracking()
     }
   }
 }
@@ -35,16 +39,22 @@ class TrackWalkViewController: UIViewController {
 // MARK: - Location manager delegate
 
 extension TrackWalkViewController: CLLocationManagerDelegate {
+  
   func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
     if status == .authorizedAlways {
-      manager.startUpdatingLocation()
+      startTracking()
     }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    print(locations[0].coordinate)
   }
 }
 
 // MARK: - Private helper methods
 
 private extension TrackWalkViewController {
+  
   func displayLocationError() {
     let alert = UIAlertController(title: "Location services are disabled",
                                   message: "Please enable location services in the Settings app in order to track your walks.",
@@ -59,5 +69,23 @@ private extension TrackWalkViewController {
     alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
     
     self.present(alert, animated: true, completion: nil)
+  }
+  
+  func requestLocation() {
+    let status = CLLocationManager.authorizationStatus()
+    
+    // Request user's location, if permission is denied then display error
+    if status == .notDetermined {
+      locationManager.requestAlwaysAuthorization()
+    } else if status == .denied {
+      displayLocationError()
+    }
+    
+  }
+  
+  func startTracking() {
+    locationManager.startUpdatingLocation()
+    mapView.showsUserLocation = true
+    mapView.userTrackingMode = .follow
   }
 }
