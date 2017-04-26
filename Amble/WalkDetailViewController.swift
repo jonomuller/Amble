1dc95ee42be5744dd74b91ec1d3b4926b474717e
Jono Muller <jonomuller@gmail.com>
2017-04-26 02:33:34 +0100

    Create class and storyboard for walk detail view

---
+++ Amble/WalkDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WalkDetailViewController.swift
+//  Amble
+//
+//  Created by Jono Muller on 26/04/2017.
+//  Copyright © 2017 Jonathan Muller. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class WalkDetailViewController: UIViewController {
+  
+  @IBOutlet var mapView: MKMapView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
