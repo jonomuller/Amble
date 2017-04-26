5ac521eb50e478ce4ae941830daaee4550b02d3b
Jono Muller <jonomuller@gmail.com>
2017-04-26 17:44:14 +0100

    Return from walk detail view if no ID is passed on appear

--- Amble/WalkDetailViewController.swift
+++ Amble/WalkDetailViewController.swift
@@ -8,15 +8,39 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class WalkDetailViewController: UIViewController {
   
   @IBOutlet var mapView: MKMapView!
   @IBOutlet var statsView: StatsView!
   
-  var walkID: String!
+  var walkID: String?
+  var coordinates: [CLLocationCoordinate2D] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    self.returnIfNoIDPassed()
+  }
+}
+
+// MARK: - Private helper methods
+
+private extension WalkDetailViewController {
+
+  func returnIfNoIDPassed() {
+    // Dismiss view controller if no ID is passed
+    if walkID == nil {
+      let noIDAlertView = UIAlertController(title: "Display error",
+                                            message: "No walk ID has been given",
+                                            preferredStyle: .alert)
+      
+      noIDAlertView.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+        self.dismiss(animated: true, completion: nil)
+      }))
+      
+      self.present(noIDAlertView, animated: true, completion: nil)
+    }
   }
 }
