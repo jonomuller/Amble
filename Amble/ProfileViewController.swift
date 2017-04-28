4c17ba603f1dc5a7af4b15ff2c1f791cf98286b9
Jono Muller <jonomuller@gmail.com>
2017-04-28 22:44:41 +0100

    Create collection view to display walks on profile

--- Amble/ProfileViewController.swift
+++ Amble/ProfileViewController.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  
+  @IBOutlet var collectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
 }
 
