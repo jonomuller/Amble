f2d126512509008be3529429d02174a6f0bed0fe
Jono Muller <jonomuller@gmail.com>
2017-04-29 19:44:07 +0100

    Create class for custom walk collection view cell Cell contains elements for walk map view, name, date and distance

---
+++ Amble/WalkCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WalkCollectionViewCell.swift
+//  Amble
+//
+//  Created by Jono Muller on 29/04/2017.
+//  Copyright © 2017 Jonathan Muller. All rights reserved.
+//
+
+import UIKit
+
+class WalkCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var distanceLabel: UILabel!
+  @IBOutlet var dateLabel: UILabel!
+  
+}
