17943e9ae1d0a7568d2bcf6d90312da60bf4f10e
Jono Muller <jonomuller@gmail.com>
2017-04-30 21:28:51 +0100

    Re-implement createWalk function to upload image first Function first gets signed AWS S3 URL from API Then uploads image via this URL Then creates walk in database as before

--- Amble/APIManager/APIManager.swift
+++ Amble/APIManager/APIManager.swift
@@ -15,46 +15,6 @@ class APIManager: NSObject {
   
   public static let sharedInstance = APIManager()
   
-  // MARK: - Private helper functions
-  
-  private func request(router: Router, completion: @escaping (APIResponse) -> Void) {
-    if let error = containsEmptyElement(details: router.parameters) {
-      completion(.failure(error: error))
-      return
-    }
-    
-    Alamofire.request(router)
-      .validate()
-      .responseJSON { (response) in
-        switch response.result {
-        case .success(let value):
-          completion(.success(json: JSON(value)))
-        case .failure:
-          if let data = response.data {
-            let json = JSON(data)
-            let description = router.path.replacingOccurrences(of: "/auth/", with: "").capitalized + " error"
-            let reason = json["error"].stringValue
-            let error = NSError(domain: "Amble",
-                                code: (response.response?.statusCode)!,
-                                userInfo: [NSLocalizedDescriptionKey: description, NSLocalizedFailureReasonErrorKey: reason])
-            completion(.failure(error: error))
-          }
-        }
-    }
-  }
-  
-  private func containsEmptyElement(details: [String: Any]) -> NSError? {
-    for (key, value) in details {
-      if String(describing: value).isEmpty {
-        return NSError(domain: "Amble",
-                       code: 400,
-                       userInfo: [NSLocalizedDescriptionKey: "Please enter your \(key)."])
-      }
-    }
-    
-    return nil
-  }
-  
   // MARK: - /auth API calls
   
   public func login(username: String, password: String, completion: @escaping (APIResponse) -> Void) {
@@ -78,21 +38,29 @@ class APIManager: NSObject {
   
   // MARK: - /walks API calls
   
-  public func createWalk(name: String, owner: String, locations: [CLLocation], time: Int, distance: Double, steps: Double, completion: @escaping (APIResponse) -> Void) {
-    var coordinates: [[Double]] = []
-    for location in locations {
-      coordinates.append([location.coordinate.longitude, location.coordinate.latitude])
-    }
+  public func createWalk(name: String, owner: String, locations: [CLLocation], image: UIImage?, time: Int, distance: Double, steps: Double, completion: @escaping (APIResponse) -> Void) {
     
-    let details = ["name": name,
-                   "owner": owner,
-                   "coordinates": coordinates.description,
-                   "time": time,
-                   "distance": distance,
-                   "steps": steps] as [String : Any]
-    
-    request(router: .createWalk(details: details)) { (response) in
-      completion(response)
+    request(router: .getMapImageURL) { (response) in
+      switch response {
+      case .success(let json):
+        let url = json["url"].stringValue
+        if let imageData = UIImageJPEGRepresentation(image!, 0.1) {
+          self.upload(data: imageData, url: url, completion: { (uploadResponse) in
+            switch uploadResponse {
+            case .success:
+              // Create walk
+              let imageURL = url
+              self.createWalk(name: name, owner: owner, locations: locations, image: imageURL, time: time, distance: distance, steps: steps, completion: { (createResponse) in
+                completion(createResponse)
+              })
+            case .failure:
+              completion(uploadResponse)
+            }
+          })
+        }
+      case .failure:
+        completion(response)
+      }
     }
   }
   
@@ -108,5 +76,85 @@ class APIManager: NSObject {
     request(router: .getWalks(id: id)) { (response) in
       completion(response)
     }
+  }
+  
+  
+  
+  // MARK: - Private helper functions
+  
+  private func request(router: Router, completion: @escaping (APIResponse) -> Void) {
+    if let error = containsEmptyElement(details: router.parameters) {
+      completion(.failure(error: error))
+      return
+    }
+    
+    Alamofire.request(router)
+      .validate()
+      .responseJSON { (response) in
+        switch response.result {
+        case .success(let value):
+          completion(.success(json: JSON(value)))
+        case .failure:
+          if let data = response.data {
+            let json = JSON(data)
+            let description = router.path.replacingOccurrences(of: "/auth/", with: "").capitalized + " error"
+            let reason = json["error"].stringValue
+            let error = NSError(domain: "Amble",
+                                code: (response.response?.statusCode)!,
+                                userInfo: [NSLocalizedDescriptionKey: description,
+                                           NSLocalizedFailureReasonErrorKey: reason])
+            completion(.failure(error: error))
+          }
+        }
+    }
+  }
+  
+  public func upload(data: Data, url: String, completion: @escaping (APIResponse) -> Void) {
+    Alamofire.upload(data, to: url, method: .put, headers: ["Content-Type":"image/jpeg"])
+      .validate()
+      .responseString { (response) in
+        print(response)
+        switch response.result {
+        case .success:
+          completion(.success(json: JSON("{\"success\": true}")))
+        case .failure:
+          let error = NSError(domain: "Amble",
+                              code: (response.response?.statusCode)!,
+                              userInfo: [NSLocalizedDescriptionKey: "Upload Error"])
+          completion(.failure(error: error))
+        }
+    }
+  }
+  
+  private func createWalk(name: String, owner: String, locations: [CLLocation], image: String, time: Int, distance: Double, steps: Double, completion: @escaping (APIResponse) -> Void) {
+    var coordinates: [[Double]] = []
+    for location in locations {
+      coordinates.append([location.coordinate.longitude, location.coordinate.latitude])
+    }
+    
+    let details = ["name": name,
+                   "owner": owner,
+                   "coordinates": coordinates.description,
+                   "image": image,
+                   "time": time,
+                   "distance": distance,
+                   "steps": steps] as [String : Any]
+    
+    request(router: .createWalk(details: details)) { (response) in
+      completion(response)
+    }
+  }
+  
+  
+  private func containsEmptyElement(details: [String: Any]) -> NSError? {
+    for (key, value) in details {
+      if String(describing: value).isEmpty {
+        return NSError(domain: "Amble",
+                       code: 400,
+                       userInfo: [NSLocalizedDescriptionKey: "Please enter your \(key)."])
+      }
+    }
+    
+    return nil
   }
 }
