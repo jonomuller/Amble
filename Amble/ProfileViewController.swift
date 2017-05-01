8d3b3255b9165623222780cd83513bbfea67ffaf
Jono Muller <jonomuller@gmail.com>
2017-05-01 06:27:54 +0100

    Implement collection view data source methods for profile Retrieve walk details from getWalks API call when view loads New WalkInfo struct created

--- Amble/ProfileViewController.swift
+++ Amble/ProfileViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ProfileViewController: UIViewController {
   
   @IBOutlet var collectionView: UICollectionView!
   
+  fileprivate var walks: [WalkInfo] = []
+  fileprivate let WALK_CELL_IDENTIFIER = "WalkCell"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    self.getWalks()
   }
 }
 
+// MARK: - Collection view data source
+
+extension ProfileViewController: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 3
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WALK_CELL_IDENTIFIER, for: indexPath) as! WalkCollectionViewCell
+    cell.nameLabel.text = "Test"
+    return cell
+  }
+}
+
+// MARK: - Private helper methods
+
+private extension ProfileViewController {
+  func getWalks() {
+    APIManager.sharedInstance.getWalks(id: (User.sharedInstance.userInfo?.id)!) { (response) in
+      switch response {
+      case .success(let json):
+        for (_, subJson): (String, JSON) in json["walks"] {
+          let walk = WalkInfo(id: subJson["id"].stringValue,
+                              name: subJson["name"].stringValue,
+                              image: subJson["image"].stringValue,
+                              date: subJson["createdAt"].stringValue)
+          self.walks.append(walk)
+        }
+        
+        self.collectionView.reloadData()
+      case .failure(let error):
+        let alertView = UIAlertController(title: error.localizedDescription,
+                                          message: error.localizedFailureReason,
+                                          preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertView, animated: true, completion: nil)
+      }
+    }
+  }
+}
