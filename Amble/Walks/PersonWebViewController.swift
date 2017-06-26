c85739e40c969451d0e5fd6722500548ed7c058f
Jono Muller <jonomuller@gmail.com>
2017-06-26 01:08:30 +0100

    Add toolbar to plaque web view

--- Amble/Walks/PersonWebViewController.swift
+++ Amble/Walks/PersonWebViewController.swift
@@ -12,6 +12,7 @@ import NVActivityIndicatorView
 class PersonWebViewController: UIViewController {
   
   @IBOutlet var webView: UIWebView!
+  @IBOutlet var toolbar: UIToolbar!
   
   var url: URL!
   var name: String!
@@ -23,6 +24,11 @@ class PersonWebViewController: UIViewController {
     
     self.navigationItem.title = name
     self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonPressed))
+    
+    let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+    let openButton = UIBarButtonItem(title: "Open in Safari", style: .done, target: self, action: #selector(openInSafari))
+    
+    toolbar.items = [flexibleSpace, openButton]
     
     webView.loadRequest(URLRequest(url: url))
     spinner = self.view.createIndicatorView(width: 50, height: 50)
@@ -46,7 +52,14 @@ extension PersonWebViewController: UIWebViewDelegate {
 // MARK: - Action methods
 
 extension PersonWebViewController {
+  
   func doneButtonPressed() {
     self.dismiss(animated: true, completion: nil)
+  }
+  
+  func openInSafari() {
+    if let url = webView.request?.url {
+      UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
   }
 }
